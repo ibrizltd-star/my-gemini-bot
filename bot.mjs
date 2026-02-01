7552b062199966ff0ded31a7be96f8db80d0730b
ibrizltd-star <ibrizltd@gmail.com>
2026-02-01 23:45:20 +0200

    Update bot.mjs

--- bot.mjs
+++ bot.mjs
@@ -1,9 +1,13 @@
 
+
 import TelegramBot from 'node-telegram-bot-api';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import http from 'http';
 
-http.createServer((req, res) => res.end('Bot is running')).listen(process.env.PORT || 3000);
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is alive');
+}).listen(process.env.PORT || 3000);
 
 const token = '8546121789:AAGviVwAPr1Fu4_Wvd2iFDMbJzc0QGw0mpg';
 const genAI = new GoogleGenerativeAI('AIzaSyCePxAxFCzUmIK5GxxzsErEmvG_ztDBzp8');
@@ -13,16 +17,17 @@ const bot = new TelegramBot(token, { polling: true });
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
+
   if (!text) return;
 
   try {
-    const model = const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const result = await model.generateContent(text);
     const response = await result.response;
     bot.sendMessage(chatId, response.text());
   } catch (error) {
-    bot.sendMessage(chatId, "Ошибка: " + error.message);
+    bot.sendMessage(chatId, "Error: " + error.message);
   }
 });
 
-console.log('--- БОТ ВКЛЮЧЕН (PRO ВЕРСИЯ) ---');
+console.log('--- BOT START ---');
