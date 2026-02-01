cc4c210db961103c581c5aaaca3b836b3bb530a2
ibrizltd-star <ibrizltd@gmail.com>
2026-02-01 23:20:46 +0200

    Update bot.mjs

--- bot.mjs
+++ bot.mjs
@@ -14,7 +14,7 @@ bot.on('message', async (msg) => {
   const text = msg.text;
 
   try {
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({model: "gemini-1.5-pro"});
     const result = await model.generateContent(text);
     const response = await result.response;
     bot.sendMessage(chatId, response.text());
